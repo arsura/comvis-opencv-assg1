15a295307f221e2a60970f4fe7bc0e7e3a777d23
Siwakorn <siwakornr58@email.nu.ac.th>
2018-09-20 03:47:38 +0700

    RGB Detect

--- comvis-opencv-assg1/main.cpp
+++ comvis-opencv-assg1/main.cpp
@@ -9,12 +9,23 @@ int main(int, char **)
 	} 
 
 	cv::Mat edges;
-	cv::namedWindow("edges", 1);
+	cv::namedWindow("Color", 1);
 	for (;;)
 	{
 		cv::Mat frame;
 		cap >> frame; // get a new frame from camera
-		cv::imshow("edges", frame);
+		double centerFrameY = (frame.rows / 2) - 100;
+		double centerFrameX = (frame.cols / 2) - 100;
+		int R = frame.at<cv::Vec3b>(frame.cols / 2, frame.rows / 2)[2];
+		int G = frame.at<cv::Vec3b>(frame.cols / 2, frame.rows / 2)[1];
+		int B = frame.at<cv::Vec3b>(frame.cols / 2, frame.rows / 2)[0];
+
+		// std::cout << "frame.cols=" << frame.cols << " " << "centerFrameX=" << centerFrameX << std::endl << std::endl;
+		// std::cout << "frame.rows=" << frame.rows << " " << "centerFrameY=" << centerFrameY << std::endl;
+		std::cout << "R=" << R << " " << "G=" << G << " " << "B=" << " " << B << std::endl;
+
+		cv::rectangle(frame, cv::Rect(centerFrameX, centerFrameY, 200, 200), CV_RGB(255, 0, 0), 2);
+		cv::imshow("Color", frame);
 		if (cv::waitKey(30) >= 0)
 			break;
 	}
