e07736748bcaaafab15d6e48ed8a89cee5926b56
arsura <arsurarizen@gmail.com>
2018-09-20 18:23:43 +0700

    RGB Detect

--- comvis-opencv-assg1/main.cpp
+++ comvis-opencv-assg1/main.cpp
@@ -9,22 +9,23 @@ int main(int, char **)
 	} 
 
 	cv::Mat edges;
+	int R, G, B;
 	cv::namedWindow("Color", cv::WINDOW_AUTOSIZE);
 	for (;;)
 	{
 		cv::Mat frame;
 		cap >> frame; // get a new frame from camera
-		double centerFrameY = (frame.rows / 2) - 100;
-		double centerFrameX = (frame.cols / 2) - 100;
-		int R = frame.at<cv::Vec3b>(frame.cols / 2, frame.rows / 2)[2];
-		int G = frame.at<cv::Vec3b>(frame.cols / 2, frame.rows / 2)[1];
-		int B = frame.at<cv::Vec3b>(frame.cols / 2, frame.rows / 2)[0];
+		double centerFrameX = (frame.cols / 2);
+		double centerFrameY = (frame.rows / 2);
+		R = frame.at<cv::Vec3b>(centerFrameX, centerFrameY)[2];
+		G = frame.at<cv::Vec3b>(centerFrameX, centerFrameY)[1];
+		B = frame.at<cv::Vec3b>(centerFrameX, centerFrameY)[0];
 
 		// std::cout << "frame.cols=" << frame.cols << " " << "centerFrameX=" << centerFrameX << std::endl << std::endl;
 		// std::cout << "frame.rows=" << frame.rows << " " << "centerFrameY=" << centerFrameY << std::endl;
 		std::cout << "R=" << R << " " << "G=" << G << " " << "B=" << " " << B << std::endl;
 
-		cv::rectangle(frame, cv::Rect(centerFrameX, centerFrameY, 200, 200), CV_RGB(255, 0, 0), 2);
+		cv::rectangle(frame, cv::Rect(centerFrameX - 100, centerFrameY - 100, 200, 200), CV_RGB(R, G, B), 2);
 		cv::imshow("Color", frame);
 		if (cv::waitKey(30) >= 0)
 			break;
