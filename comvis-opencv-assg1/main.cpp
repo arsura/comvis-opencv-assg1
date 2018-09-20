403fb2e20c8ff00fc8f270072ed2b43e6c2fb433
Siwakorn <siwakornr58@email.nu.ac.th>
2018-09-21 03:20:27 +0700

    HSV

--- comvis-opencv-assg1/main.cpp
+++ comvis-opencv-assg1/main.cpp
@@ -1,36 +1,84 @@
-#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
 
-int main(int, char **)
+int main(int argc, char** argv)
 {
-	cv::VideoCapture cap(0); // open the default camera
+    // Load BGR image
+    Mat3b bgr = imread("resource/img/color_pencil.jpg");
+	Mat3b rgb;
+
+    if (bgr.empty()) 
+    {
+        cout << "Cannot open the image" << endl;
+        return -1;
+    }
+
+    // Transform to HSV
+    Mat3b hsv;
+    cvtColor(bgr, hsv, COLOR_BGR2HSV); 
+
+    // Create a window called "Control"
+    namedWindow("Control", cv::WINDOW_AUTOSIZE); 
+
+    // Set starting values for ranges
+    int iLowH = 0;
+    int iHighH = 10;
+
+    int iLowS = 145;
+    int iHighS = 255;
+
+    int iLowV = 100;
+    int iHighV = 255;
+
+    //Create trackbars in "Control" window
+    createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    createTrackbar("HighH", "Control", &iHighH, 179);
+
+    createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    createTrackbar("HighS", "Control", &iHighS, 255);
+
+    createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    createTrackbar("HighV", "Control", &iHighV, 255);
+
+    //Show the original image
+    imshow("Original", bgr); 
 	
-	if (!cap.isOpened()) {
-		return -1; // check if we succeeded
-	} 
 
-	cv::Mat edges;
-	int R, G, B;
-	cv::namedWindow("Color", cv::WINDOW_AUTOSIZE);
-	for (;;)
-	{
-		cv::Mat frame;
-		cap >> frame; // get a new frame from camera
-		double centerFrameX = (frame.cols / 2);
-		double centerFrameY = (frame.rows / 2);
-		R = frame.at<cv::Vec3b>(centerFrameX, centerFrameY)[2];
-		G = frame.at<cv::Vec3b>(centerFrameX, centerFrameY)[1];
-		B = frame.at<cv::Vec3b>(centerFrameX, centerFrameY)[0];
+    // Create kernel for morphological operation
+    Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
 
-		// std::cout << "frame.cols=" << frame.cols << " " << "centerFrameX=" << centerFrameX << std::endl << std::endl;
-		// std::cout << "frame.rows=" << frame.rows << " " << "centerFrameY=" << centerFrameY << std::endl;
-		std::cout << "R=" << R << " " << "G=" << G << " " << "B=" << " " << B << std::endl;
+    // Infinte loop, until user press "esc"
+    while (true)
+    {
+        Mat mask;
 
-		cv::rectangle(frame, cv::Rect(centerFrameX - 100, centerFrameY - 100, 200, 200), CV_RGB(R, G, B), 2);
-		cv::imshow("Color", frame);
-		if (cv::waitKey(30) >= 0)
-			break;
-	}
 
-	// the camera will be deinitialized automatically in VideoCapture destructor
-	return 0;
+        inRange(hsv, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), mask); //Threshold the image
+
+		for (int i = 0; i < mask.cols; i++) {
+			for (int j = 0; j < mask.rows; j++) {
+				if (mask.at<uchar>(j, i) == 0) {
+					// cout << j << " " << i << endl << endl;
+				}
+			}
+		}
+
+        //morphological opening (remove small objects from the foreground)
+        morphologyEx(mask, mask, MORPH_OPEN, kernel);
+
+        //morphological closing (fill small holes in the foreground)
+        morphologyEx(mask, mask, MORPH_CLOSE, kernel);
+
+        //Show the thresholded image
+        imshow("Thresholded Image", mask); 
+
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+        {
+            cout << "esc key is pressed by user" << endl;
+            break;
+        }
+    }
+    return 0;
 }
