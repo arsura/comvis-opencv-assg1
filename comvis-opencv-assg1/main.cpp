f4699bb72962be392d35353ce830ffeb6a32925a
arsura <arsurarizen@gmail.com>
2018-09-25 14:25:22 +0700

    find hsv

--- comvis-opencv-assg1/main.cpp
+++ comvis-opencv-assg1/main.cpp
@@ -1,76 +1,74 @@
-#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
 #include <iostream>
-
 using namespace cv;
-using namespace std;
-
-int main(int argc, char** argv)
+const int max_value_H = 360 / 2;
+const int max_value = 255;
+const String window_capture_name = "Video Capture";
+const String window_detection_name = "Object Detection";
+int low_H = 0, low_S = 0, low_V = 0;
+int high_H = max_value_H, high_S = max_value, high_V = max_value;
+static void on_low_H_thresh_trackbar(int, void *)
 {
-    // Load BGR image
-    Mat3b bgr = imread("resource/img/gray_shade.png");
-	Mat3b rgb;
-
-    if (bgr.empty()) 
-    {
-        cout << "Cannot open the image" << endl;
-        return -1;
-    }
-
-    // Transform to HSV
-    Mat3b hsv;
-    cvtColor(bgr, hsv, COLOR_BGR2HSV); 
-
-    // Create a window called "Control"
-    namedWindow("Control", cv::WINDOW_AUTOSIZE); 
-
-    // Set starting values for ranges
-    int iLowH = 0;
-    int iHighH = 10;
-
-    int iLowS = 145;
-    int iHighS = 255;
-
-    int iLowV = 100;
-    int iHighV = 255;
-
-    //Create trackbars in "Control" window
-    createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-    createTrackbar("HighH", "Control", &iHighH, 179);
-
-    createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-    createTrackbar("HighS", "Control", &iHighS, 255);
-
-    createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
-    createTrackbar("HighV", "Control", &iHighV, 255);
-
-    //Show the original image
-    imshow("Original", bgr); 
-	
-
-    // Create kernel for morphological operation
-    Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
-
-    // Infinte loop, until user press "esc"
-    while (true)
-    {
-        Mat mask;
-
-        inRange(hsv, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), mask); //Threshold the image
-
-        //morphological opening (remove small objects from the foreground)
-        morphologyEx(mask, mask, MORPH_OPEN, kernel);
-
-        //morphological closing (fill small holes in the foreground)
-        morphologyEx(mask, mask, MORPH_CLOSE, kernel);
-
-        //Show the thresholded image
-        imshow("Thresholded Image", mask); 
-
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-        {
-            cout << "esc key is pressed by user" << endl;
-            break;
-        }
-    }
-    return 0;
+	low_H = min(high_H - 1, low_H);
+	setTrackbarPos("Low H", window_detection_name, low_H);
+}
+static void on_high_H_thresh_trackbar(int, void *)
+{
+	high_H = max(high_H, low_H + 1);
+	setTrackbarPos("High H", window_detection_name, high_H);
+}
+static void on_low_S_thresh_trackbar(int, void *)
+{
+	low_S = min(high_S - 1, low_S);
+	setTrackbarPos("Low S", window_detection_name, low_S);
+}
+static void on_high_S_thresh_trackbar(int, void *)
+{
+	high_S = max(high_S, low_S + 1);
+	setTrackbarPos("High S", window_detection_name, high_S);
+}
+static void on_low_V_thresh_trackbar(int, void *)
+{
+	low_V = min(high_V - 1, low_V);
+	setTrackbarPos("Low V", window_detection_name, low_V);
+}
+static void on_high_V_thresh_trackbar(int, void *)
+{
+	high_V = max(high_V, low_V + 1);
+	setTrackbarPos("High V", window_detection_name, high_V);
+}
+int main(int argc, char* argv[])
+{
+	VideoCapture cap(argc > 1 ? atoi(argv[1]) : 0);
+	namedWindow(window_capture_name);
+	namedWindow(window_detection_name);
+	// Trackbars to set thresholds for HSV values
+	createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
+	createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
+	createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
+	createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
+	createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
+	createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
+	Mat frame, frame_HSV, frame_threshold;
+	while (true) {
+		cap >> frame;
+		if (frame.empty())
+		{
+			break;
+		}
+		// Convert from BGR to HSV colorspace
+		cvtColor(frame, frame_HSV, COLOR_BGR2HSV);
+		// Detect the object based on HSV Range Values
+		inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+		// Show the frames
+		imshow(window_capture_name, frame);
+		imshow(window_detection_name, frame_threshold);
+		char key = (char)waitKey(30);
+		if (key == 'q' || key == 27)
+		{
+			break;
+		}
+	}
+	return 0;
 }
