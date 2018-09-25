c970d62c58c514a03cba825e4c12b98a36ca4b01
Siwakorn <siwakornr58@email.nu.ac.th>
2018-09-26 02:26:38 +0700

    detect color

--- comvis-opencv-assg1/main.cpp
+++ comvis-opencv-assg1/main.cpp
@@ -1,70 +1,59 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <iostream>
-using namespace cv;
-const int max_value_H = 360 / 2;
-const int max_value = 255;
-const String window_capture_name = "Video Capture";
-const String window_detection_name = "Object Detection";
-int low_H = 0, low_S = 0, low_V = 0;
-int high_H = max_value_H, high_S = max_value, high_V = max_value;
-static void on_low_H_thresh_trackbar(int, void *)
-{
-	low_H = min(high_H - 1, low_H);
-	setTrackbarPos("Low H", window_detection_name, low_H);
-}
-static void on_high_H_thresh_trackbar(int, void *)
-{
-	high_H = max(high_H, low_H + 1);
-	setTrackbarPos("High H", window_detection_name, high_H);
-}
-static void on_low_S_thresh_trackbar(int, void *)
-{
-	low_S = min(high_S - 1, low_S);
-	setTrackbarPos("Low S", window_detection_name, low_S);
-}
-static void on_high_S_thresh_trackbar(int, void *)
-{
-	high_S = max(high_S, low_S + 1);
-	setTrackbarPos("High S", window_detection_name, high_S);
-}
-static void on_low_V_thresh_trackbar(int, void *)
-{
-	low_V = min(high_V - 1, low_V);
-	setTrackbarPos("Low V", window_detection_name, low_V);
-}
-static void on_high_V_thresh_trackbar(int, void *)
-{
-	high_V = max(high_V, low_V + 1);
-	setTrackbarPos("High V", window_detection_name, high_V);
-}
+
+
 int main(int argc, char* argv[])
 {
-	VideoCapture cap(argc > 1 ? atoi(argv[1]) : 0);
-	namedWindow(window_capture_name);
-	namedWindow(window_detection_name);
-	// Trackbars to set thresholds for HSV values
-	createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
-	createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
-	createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
-	createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
-	createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
-	createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
-	Mat frame, frame_HSV, frame_threshold;
+	cv::VideoCapture cap(argc > 1 ? atoi(argv[1]) : 0);
+
+	cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("HSV", cv::WINDOW_AUTOSIZE);
+	
+	cv::Mat frame, frame_HSV, frame_threshold;
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
+	int nonZero;
+
 	while (true) {
+
 		cap >> frame;
-		if (frame.empty())
-		{
+		if (frame.empty()){
 			break;
 		}
 		// Convert from BGR to HSV colorspace
-		cvtColor(frame, frame_HSV, COLOR_BGR2HSV);
-		// Detect the object based on HSV Range Values
-		inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+		cv::cvtColor(frame, frame_HSV, cv::COLOR_BGR2HSV);
+		
+		cv::Point center(frame_threshold.cols / 2, frame_threshold.rows / 2);
+
+		
+		cv::inRange(frame_HSV, cv::Scalar(90, 80, 90), cv::Scalar(110, 240, 255), frame_threshold);
+		cv::morphologyEx(frame_threshold, frame_threshold, cv::MORPH_OPEN, kernel);
+		cv::morphologyEx(frame_threshold, frame_threshold, cv::MORPH_CLOSE, kernel);
+
+		nonZero = cv::countNonZero(frame_threshold);
+		std::cout << nonZero << std::endl;
+		if (nonZero > 1000) {
+			cv::putText(frame, "Blue", center, 1, 2.0, cv::Scalar(255, 0, 0), 1);
+		}
+
+
+		cv::inRange(frame_HSV, cv::Scalar(20, 130, 130), cv::Scalar(50, 255, 255), frame_threshold);
+		cv::morphologyEx(frame_threshold, frame_threshold, cv::MORPH_OPEN, kernel);
+		cv::morphologyEx(frame_threshold, frame_threshold, cv::MORPH_CLOSE, kernel);
+
+		nonZero = cv::countNonZero(frame_threshold);
+		std::cout << nonZero << std::endl;
+		if (nonZero > 1000) {
+			cv::putText(frame, "Yellow", center, 1, 2.0, cv::Scalar(0,255,255), 1);
+		}
+
+
+
 		// Show the frames
-		imshow(window_capture_name, frame);
-		imshow(window_detection_name, frame_threshold);
-		char key = (char)waitKey(30);
+		cv::imshow("Original", frame);
+		cv::imshow("HSV", frame_threshold);
+
+		char key = (char)cv::waitKey(30);
 		if (key == 'q' || key == 27)
 		{
 			break;
